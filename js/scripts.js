6577fc37d78a96c3a49033b7821bca268590ac92
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-06-21 08:34:44 -0700

    Add pizza object been mostly brainstorming how the back end and front end will look

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Array
+function PizzaOptions() {
+  this.toppings = [],
+
+}
+//Objects
+function Pizza(topping, crust, size) {
+  this.topping = topping,
+  this.crust = crust,
+  this.size = size
+}
